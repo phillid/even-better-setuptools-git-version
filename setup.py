8593d8de2797f973c0f5e36a3312cab3c2173a67
Vivin Paliath <vivin.paliath@gmail.com>
2018-12-12 12:10:56 -0700

    Changes:
     - Support for starting_version if no tags are present.
     - Only look for tags reachable from HEAD.
     - Add a -dirty suffix if working tree is dirty or contains untracked files.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 def get_tag():
     """Return the last tag for the git repository."""
-    return subprocess.getoutput('git tag --sort=version:refname | tail -n1')
+    return subprocess.getoutput('git tag --sort=version:refname --merged | tail -n1')
 
 
 def README():
@@ -15,12 +15,12 @@ def README():
 
 
 setup(
-    name='very-good-setuptools-git-version',
+    name='better-setuptools-git-version',
     version=get_tag(),
-    url='https://github.com/Kautenja/very-good-setuptools-git-version',
-    author='Chritian Kauten',
-    author_email='kautencreations@gmail.com',
-    description='Automatically set package version from Git.',
+    url='https://github.com/vivin/better-setuptools-git-version',
+    author='Vivin Paliath',
+    author_email='vivin.paliath@gmail.com',
+    description='Automatically set package version using git tags.',
     long_description=README(),
     long_description_content_type='text/markdown',
     keywords='setuptools git version-control',
@@ -33,16 +33,16 @@ setup(
         'Programming Language :: Python :: 3',
         'Development Status :: 5 - Production/Stable',
     ],
-    py_modules=['very_good_setuptools_git_version'],
+    py_modules=['better_setuptools_git_version'],
     install_requires=[
         'setuptools >= 8.0',
     ],
     entry_points={
         'distutils.setup_keywords': [
-            'version_format = very_good_setuptools_git_version:validate_version_format'
+            'version_config = better_setuptools_git_version:validate_version_config'
         ],
         'console_scripts': [
-            'very-good-setuptools-git-version = very_good_setuptools_git_version:get_version'
+            'better-setuptools-git-version = better_setuptools_git_version:get_version'
         ]
     }
 )
